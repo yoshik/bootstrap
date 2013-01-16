36a4d12b5e6d28b58443c6774daac80c5f8f4446
yoshik <fujiwara.access@gmail.com>
2013-01-16 22:17:43 +0900

    font awesomeのbuild

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
+cp Font-Awesome/less/font-awesome.less less/
+cp less/bootstrap.less less/bootstrap.less.copy
+sed 's/@import "sprites.less";/@import "font-awesome.less";/g' less/bootstrap.less.copy >less/bootstrap.less
+rm less/bootstrap.less.copy 
 export PATH=$PATH:$PWD/bin
 make
