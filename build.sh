cc771e21dd5992a4b36a9d6f5c95850163734f85
yoshik <fujiwara.access@gmail.com>
2013-01-17 16:19:22 +0900

    min.cssのbuild

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ sed 's/@import "sprites.less";/@import "font-awesome.less";/g' less/bootstrap.le
 rm less/bootstrap.less.copy 
 export PATH=$PATH:$PWD/bin
 make
+lessc --compress ./less/bootstrap.less out/bootstrap.min.css
+lessc --compress ./less/responsive.less out/bootstrap-responsive.min.css
 perl bs2tsr.pl
 cp docs/assets/js/bootstrap.min.js out/
 cp Font-Awesome/css/font-awesome.min.css out/
